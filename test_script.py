e7e84d907e744e0f74fd05cdb98095a76228b8c3
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-07-11 13:31:37 -0300

    :wrench: | test_script.py

--- test_script.py
+++ test_script.py
@@ -1,14 +1,15 @@
 from os import system
-
 commands = [
+    'rm -r *.tsv',
     'clear',
-    'python taxamTestGenerator -n pool_esc_a -s A,B -nt 9,9,9,9,9,9,9 -pt 0 -nr 1000 -nc 1000 -pm 0.85 -tr 3000 -tc 1000 -cr 0.75 -cc 0.90 -mc 0.65 > output.txt',
+    'python taxamTestGenerator -n pool_esc_a -s A,B -nt 9,9,9,9,9,9,9 -pt 0 -nr 100 -nc 20 -pm 0.85 -tr 100 -tc 100 -cr 0.75 -cc 0.90 -mc 0.65 > output.txt',
     'rm -r pool_tests',
     'mv pool_esc_a pool_tests'
 ]
 
-for command in commands:
+for key, command in enumerate(commands):
     try:
         system(command)
+        print(f'{key} -> {command}')
     except Exception as e:
         print(e)
